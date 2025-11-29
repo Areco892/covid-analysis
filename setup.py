e416ac4f5a72e7efe1e7f9bd02d98da8fda08cc3
Alexander Huang <ahuangliu@ucsd.edu>
2025-11-29 14:28:55 -0800

    Added the change from excel to csv of the CovidDeaths file. in the setup.py file.

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
+df = pd.read_excel("CovidDeaths.xlsx")
+df.to_csv("CovidDeaths.csv", index=False)
+
 df = pd.read_excel("CovidVaccinations.xlsx")
 df.to_csv("CovidVaccinations.csv", index=False)
